d39fe9d95d62f991022c72c12a2bf5ca3c6b585c
Grant Benson <grantjbenson@icloud.com>
2024-06-20 17:27:21 -0400

    added online subsystems eos extension to build file

--- multiTemplate/Source/multiTemplate/multiTemplate.Build.cs
+++ multiTemplate/Source/multiTemplate/multiTemplate.Build.cs
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class multiTemplate : ModuleRules
 {
@@ -18,6 +19,18 @@ public class multiTemplate : ModuleRules
 		// Uncomment if you are using online features
 		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        //added 6/20/24 by gjb
+        //add OnlineSubsystemEOS to extend upon the existing subsystem
+        PrivateDependencyModuleNames.Add("OnlineSubsystemEOS");
+        // Specify path to your SDK's Include directory
+        string SDKPath = Path.Combine(ModuleDirectory, "../../ThirdParty/SDK");
+        PublicIncludePaths.Add(Path.Combine(SDKPath, "Include"));
+
+        // Add libraries to link against
+        PublicAdditionalLibraries.Add(Path.Combine(SDKPath, "Lib", "EOSSDK-Win64-Shipping.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(SDKPath, "Lib", "EOSSDK-Win32-Shipping.lib"));
+        //end of section added 6/20/24 by gjb
+    }
 }
